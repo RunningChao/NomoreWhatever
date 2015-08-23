c88d3a54643c703610336f1c911d13014939837e
Andy Wang <andychw@yahoo-inc.com>
2015-08-23 17:09:50 +0800

    use Palette API

--- app/src/main/java/com/yahoo/android/nomorewhatever/activity/MainActivity.java
+++ app/src/main/java/com/yahoo/android/nomorewhatever/activity/MainActivity.java
@@ -1,12 +1,15 @@
 package com.yahoo.android.nomorewhatever.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.yahoo.android.nomorewhatever.R;
 import com.yahoo.android.nomorewhatever.adapter.PlaceTypeListAdapter;
@@ -34,8 +37,22 @@ public class MainActivity extends Activity {
     mAdapter = new PlaceTypeListAdapter(this);
     mRecyclerView.setAdapter(mAdapter);
 
+    mAdapter.setOnItemClickListener( new PlaceTypeListAdapter.OnItemClickListener(){
+
+      @Override
+      public void onItemClick(View view, int position) {
+        Toast.makeText(MainActivity.this, "Clicked " + position, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+        intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
+        startActivity(intent);
+      }
+    } );
+
     isListView = true;
   }
+
+
+
 
   private void setUpActionBar() {
 
@@ -63,10 +80,12 @@ public class MainActivity extends Activity {
   private void toggle() {
     MenuItem item = menu.findItem(R.id.action_toggle);
     if (isListView) {
+      mStaggeredLayoutManager.setSpanCount(2);
       item.setIcon(R.drawable.ic_action_list);
       item.setTitle("Show as list");
       isListView = false;
     } else {
+      mStaggeredLayoutManager.setSpanCount(1);
       item.setIcon(R.drawable.ic_action_grid);
       item.setTitle("Show as grid");
       isListView = true;
--- app/src/main/java/com/yahoo/android/nomorewhatever/adapter/PlaceTypeListAdapter.java
+++ app/src/main/java/com/yahoo/android/nomorewhatever/adapter/PlaceTypeListAdapter.java
@@ -1,6 +1,9 @@
 package com.yahoo.android.nomorewhatever.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -33,10 +36,20 @@ public class PlaceTypeListAdapter extends RecyclerView.Adapter<PlaceTypeListAdap
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
         final Place place = new PlaceData().placeList().get(position);
+
         holder.placeName.setText(place.name);
         Picasso.with(mContext).load(place.getImageResourceId(mContext)).into(holder.placeImage);
+
+        Bitmap photo = BitmapFactory.decodeResource(mContext.getResources(), place.getImageResourceId(mContext));
+
+        Palette.generateAsync(photo, new Palette.PaletteAsyncListener() {
+            public void onGenerated(Palette palette) {
+                int mutedLight = palette.getMutedColor(mContext.getResources().getColor(android.R.color.black));
+                holder.placeNameHolder.setBackgroundColor(mutedLight);
+            }
+        });
     }
 
     @Override
