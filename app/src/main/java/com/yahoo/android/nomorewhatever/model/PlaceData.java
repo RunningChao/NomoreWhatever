b7251f7894c37b775c5019d1ff9048e59e54c891
Andy Wang <andychw@yahoo-inc.com>
2015-08-26 00:55:04 +0800

    add db functionalities

--- app/src/main/java/com/yahoo/android/nomorewhatever/model/PlaceData.java
+++ app/src/main/java/com/yahoo/android/nomorewhatever/model/PlaceData.java
@@ -6,17 +6,20 @@ public class PlaceData {
 
   public static String[] placeNameArray = {"Bora Bora", "Canada", "Dubai", "Hong Kong", "Iceland", "India", "Kenya", "London", "Switzerland", "Sydney"};
 
-  public static ArrayList<Place> placeList() {
-    ArrayList<Place> list = new ArrayList<>();
+  public static ArrayList<PlaceType> placeList() {
+    ArrayList<PlaceType> list = new ArrayList<>();
     for (int i = 0; i < placeNameArray.length; i++) {
-      Place place = new Place();
+      PlaceType place = new PlaceType(placeNameArray[i]);
       place.name = placeNameArray[i];
       place.imageName = placeNameArray[i].replaceAll("\\s+", "").toLowerCase();
       if (i == 2 || i == 5) {
         place.isFav = true;
       }
+
       list.add(place);
     }
     return (list);
   }
+
+
 }
