8f88c2d4305bed5614d5879681812339f01f8a50
mchsieh <mchsieh@yahoo-inc.com>
2015-08-29 15:22:25 +0800

    add luckyabordactivity

---
+++ app/src/main/java/com/yahoo/android/nomorewhatever/activity/LuckyBoardActivity.java
@@ -0,0 +1,74 @@
+package com.yahoo.android.nomorewhatever.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.yahoo.android.nomorewhatever.R;
+import com.yahoo.android.nomorewhatever.ui.LuckyPanView;
+
+public class LuckyBoardActivity extends Activity {
+
+    private ImageView mStartBtn;
+    private LuckyPanView mLuckyPanView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lucky_board);
+
+        mLuckyPanView = (LuckyPanView) findViewById(R.id.id_luckypan);
+        mLuckyPanView.setListener(new LuckyPanView.onClickLuckyPanListener() {
+            @Override
+            public void onClickLuckyPan(int luckyId) {
+                Toast.makeText(LuckyBoardActivity.this, "click item with id " + luckyId, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mStartBtn = (ImageView) findViewById(R.id.id_start_btn);
+        mStartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!mLuckyPanView.isStart()) {
+                    // TODO :change icon layout here
+                    // mStartBtn.setImageResource(R.drawable.stop);
+                    mLuckyPanView.luckyStart((int) Math.floor(Math.random() * 6));
+                } else {
+                    if (!mLuckyPanView.isShouldEnd()) {
+                        // mStartBtn.setImageResource(R.drawable.start);
+                        mLuckyPanView.luckyEnd();
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
