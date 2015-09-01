815a95c200080b7c503358eb8fd1af8224be4437
mchsieh <mchsieh@yahoo-inc.com>
2015-09-01 16:50:49 +0800

    remove toast on mainactivity

--- app/src/main/java/com/yahoo/android/nomorewhatever/activity/LuckyBoardActivity.java
+++ app/src/main/java/com/yahoo/android/nomorewhatever/activity/LuckyBoardActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -32,7 +33,7 @@ public class LuckyBoardActivity extends Activity {
 
         Intent intent = getIntent();
         mPlaces = (List<Place>) intent.getSerializableExtra("selected_places");
-
+Log.d("Debug", "id:" + mPlaces.get(0).getId());
         mLuckyPanView = (LuckyPanView) findViewById(R.id.id_luckypan);
         mLuckyPanView.setPlacesonBoard(mPlaces);
 
--- app/src/main/java/com/yahoo/android/nomorewhatever/activity/MainActivity.java
+++ app/src/main/java/com/yahoo/android/nomorewhatever/activity/MainActivity.java
@@ -1,9 +1,7 @@
 package com.yahoo.android.nomorewhatever.activity;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -12,7 +10,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.yahoo.android.nomorewhatever.R;
 import com.yahoo.android.nomorewhatever.adapter.PlaceTypeListAdapter;
@@ -79,7 +76,7 @@ public class MainActivity extends Activity {
     }
 
     public void showPhoto(View view) {
-        Toast.makeText(MainActivity.this, "Clicked " + view.getId(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(MainActivity.this, "Clicked " + view.getId(), Toast.LENGTH_SHORT).show();
     }
 
 
