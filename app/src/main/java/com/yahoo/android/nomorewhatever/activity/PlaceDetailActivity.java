7a5f4532720312e5041f3c5dd6df5175841d98e5
mchsieh <mchsieh@yahoo-inc.com>
2015-09-01 17:30:32 +0800

    image remove dark effect

--- app/src/main/java/com/yahoo/android/nomorewhatever/activity/PlaceDetailActivity.java
+++ app/src/main/java/com/yahoo/android/nomorewhatever/activity/PlaceDetailActivity.java
@@ -64,7 +64,7 @@ public class PlaceDetailActivity extends Activity {
             public void onTransitionEnd(Transition transition) {
                 ImageView hero = (ImageView) findViewById(R.id.photo);
                 ObjectAnimator color = ObjectAnimator.ofArgb(hero.getDrawable(), "tint",
-                        getResources().getColor(R.color.photo_tint), 0);
+                        getResources().getColor(R.color.photo_dark), 0);
                 color.start();
 
                 findViewById(R.id.info).animate().alpha(1.0f);
